23b6983fe993ba19a3ba5d77092241ca6ac8dbf3
Jegan <jegancareer@gmail.com>
2018-05-10 18:36:09 +0200

    setup

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var AngularSpringApp = {};
+
+var App = angular.module('AngularSpringApp', ['AngularSpringApp.filters', 'AngularSpringApp.services', 'AngularSpringApp.directives','firebase']);
+
+// Declare app level module which depends on filters, and services
+App.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/cars', {
+        templateUrl: 'cars/layout',
+        controller: CarController
+    });
+
+    $routeProvider.when('/roll', {
+        templateUrl: 'roll/layout',
+        controller: RollController
+    });
+    
+    
+    $routeProvider.otherwise({redirectTo: '/cars'});
+}]);
+
+
+var config = { //Setup your Firebase API Key here
+};
+firebase.initializeApp(config);
+
+
+/*var rec = database.ref().child('RollProperty/1').once('value').then(function(snapshot) {
+	 // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+	  // ...
+	alert(' - '+ JSON.stringify(snapshot.val())) ;
+	});
+*/
+
+
